1b1081d9c9f56eb1ea289fa7b082cb334a4bebbb
Max Bond <max.o.bond@gmail.com>
2025-03-27 19:10:09 -0600

    Updated prelude to include new traits

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,16 +1,22 @@
-// Concrete types
-pub use crate::parse::{Never, NotFound, ParserError, ParserResult};
+// Concrete types & aliases
+pub use crate::parse::{
+    Incomplete, Never, NotFound, ParserError, ParserResult, StreamingError, StreamingOk,
+    StreamingResult,
+};
 
 // Implementable traits
 pub use crate::input::Input;
-pub use crate::parse::Parser;
+pub use crate::parse::{Parser, StreamingParser};
 
 // Automatic traits - these occupy common names, so bind anonymously to
 // avoid polluting namespace.
 pub use crate::{
-    parse::{Choice as _, Compose as _, FuseSequence as _, Sequence as _},
-    util::{
-        not_found::{OrIncomplete as _, OrNotFound as _},
-        rotate::{Rotate as _, RotateFn as _},
+    parse::{
+        Choice as _, Compose as _, FuseSequence as _, Sequence as _, StreamingChoice as _,
+        StreamingCompose as _,
+    },
+    util::conditional_transforms::{
+        EitherCompleteIf as _, MaybeCompleteIf as _, NoPartial as _, OrFail as _,
+        OrIncomplete as _, OrNotFound as _, StreamingOrNotFound as _,
     },
 };
